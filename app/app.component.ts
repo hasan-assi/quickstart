9c257d701794b97ab791fef9fe222b9da3d338b1
Mikhail Lukashevich <Mykhail.Lukashevych@AerData.com>
2017-01-24 11:28:07 +0100

    Added export to PDF.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,8 @@ import { applyMixins } from 'rxjs/util/applyMixins';
 import { appendFile } from 'fs';
 import { Component } from '@angular/core';
 import { Data } from './service/data.service';
-import * as html2canvas from '../node_modules/html2canvas/dist/html2canvas.js';
-
+import * as html2canvas from '../node_modules/html2canvas/dist/html2canvas';
+import * as jsPDF from '../node_modules/jspdf/dist/jspdf.debug.js';
 
 @Component({
   moduleId: module.id,
@@ -55,11 +55,50 @@ export class AppComponent {
 
   onExportButtonClick() {
     const el = document.getElementById('2Export');
-    html2canvas(el)
-      .then((canvas: any) => {
-        document.body.appendChild(canvas);
-      });
-    }
+    html2canvas(el, {
+      onrendered: (canvas: any) => {
+        const pdf = new jsPDF('p', 'pt', 'letter');
+        for (let i = 0; i <= el.clientHeight / 980; i++) {
+          const srcImg = canvas;
+          const sX = 0;
+          const sY = 980 * i; // start 980 pixels down for every new page
+          const sWidth = 900;
+          const sHeight = 980;
+          const dX = 0;
+          const dY = 0;
+          const dWidth = 900;
+          const dHeight = 980;
+
+          const onePageCanvas = document.createElement("canvas");
+          onePageCanvas.setAttribute('width', '900px');
+          onePageCanvas.setAttribute('height', '980px');
+          var ctx = onePageCanvas.getContext('2d');
+          // details on this usage of this function:
+          // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
+          ctx.drawImage(srcImg, sX, sY, sWidth, sHeight, dX, dY, dWidth, dHeight);
+
+          // document.body.appendChild(canvas);
+          var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+
+          var width = onePageCanvas.width;
+          var height = onePageCanvas.clientHeight;
+
+          //! If we're on anything other than the first page,
+          // add another page
+          if (i > 0) {
+            pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
+          }
+          //! now we declare that we're working on that page
+          pdf.setPage(i + 1);
+          //! now we add content to that page!
+          pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width * .62), (height * .62));
+
+        }
+        //! after the for loop is finished running, we save the pdf.
+        pdf.save('export.pdf');
+      }
+    });
+  }
 }
 
 class Product {
